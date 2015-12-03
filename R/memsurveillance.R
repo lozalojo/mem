bc796d3c9530bc091bb01874037db7e8d3017269
Jose E. Lozano <lozalojo@gmail.com>
2015-12-03 14:05:54 +0100

    Fixes

--- R/memsurveillance.R
+++ R/memsurveillance.R
@@ -304,5 +304,5 @@ memsurveillance<-function(i.current,
   )
   par(opar)
   dev.off()
-  cat("graph created:",getwd(),"/",i.output,"/",graph.name,".tiff","\n",sep="")
+  cat("graph created: ",getwd(),"/",i.output,"/",graph.name,".tiff","\n",sep="")
 }
