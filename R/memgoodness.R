e0b7639965248a3192129a3308743c6b8f0118e7
Jose Eugenio Lozano Alonso <LozAloJo@jcyl.red>
2017-02-03 13:39:39 +0100

    Added reference

--- R/memgoodness.R
+++ R/memgoodness.R
@@ -49,12 +49,16 @@
 #' @author Jose E. Lozano \email{lozalojo@@gmail.com}
 #'
 #' @references
-#' Vega T., Lozano J.E. (2004) Modelling influenza epidemic - can we detect the beginning
-#' and predict the intensity and duration? International Congress Series 1263 (2004)
-#' 281-283.\cr
-#' Vega T., Lozano J.E. (2012) Influenza surveillance in Europe: establishing epidemic
-#' thresholds by the Moving Epidemic Method. Influenza and Other Respiratory Viruses,
-#' DOI:10.1111/j.1750-2659.2012.00422.x.
+#' Vega Alonso, Tomas, Jose E Lozano Alonso, Raul Ortiz de Lejarazu, and Marisol Gutierrez Perez. 2004. 
+#' Modelling Influenza Epidemic: Can We Detect the Beginning and Predict the Intensity and Duration? 
+#' International Congress Series, Options for the Control of Influenza V. Proceedings of the International 
+#' Conference on Options for the Control of Influenza V, 1263 (June): 281-83. doi:10.1016/j.ics.2004.02.121.\cr
+#' Vega, Tomas, Jose Eugenio Lozano, Tamara Meerhoff, Rene Snacken, Joshua Mott, Raul Ortiz de Lejarazu, and 
+#' Baltazar Nunes. 2013. Influenza Surveillance in Europe: Establishing Epidemic Thresholds by the Moving 
+#' Epidemic Method. Influenza and Other Respiratory Viruses 7 (4): 546-58. doi:10.1111/j.1750-2659.2012.00422.x.\cr
+#' Vega, Tomas, Jose E. Lozano, Tamara Meerhoff, Rene Snacken, Julien Beaute, Pernille Jorgensen, Raul Ortiz 
+#' de Lejarazu, et al. 2015. Influenza Surveillance in Europe: Comparing Intensity Levels Calculated Using 
+#' the Moving Epidemic Method. Influenza and Other Respiratory Viruses 9 (5): 234-46. doi:10.1111/irv.12330.
 #'
 #' @keywords influenza
 #'
