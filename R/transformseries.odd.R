c61a205396ea79a235c49a006065d79be4394144
lozalojo <lozalojo@gmail.com>
2017-04-20 19:58:41 +0200

    error

--- R/transformseries.odd.R
+++ R/transformseries.odd.R
@@ -1,10 +1,10 @@
 #' fills in missing values inside the season with smoothing regression
 #'
 #' @keywords internal
-transformseries.odd<-function(x,mult){
+transformseries.odd<-function(x, mult=0){
   cat("odd multiplier: ",mult,"\n")
   x[is.nan(x)]<-NA
   x[is.infinite(x)]<-NA
-  if (all(is.na(x))) xt<-x else xt<-x/(mult-x)
+  if (all(is.na(x))) xt<-x else xt<-x/(10^mult-x)
   return(xt)
 }
