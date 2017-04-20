52232f6ad85e00b2a21fc2351f9a6243801c5bf1
lozalojo <lozalojo@gmail.com>
2017-04-20 20:25:07 +0200

    removed cat

--- R/transformseries.odd.R
+++ R/transformseries.odd.R
@@ -2,7 +2,6 @@
 #'
 #' @keywords internal
 transformseries.odd<-function(x, mult=0){
-  cat("odd multiplier: ",mult,"\n")
   x[is.nan(x)]<-NA
   x[is.infinite(x)]<-NA
   if (all(is.na(x))) xt<-x else xt<-x/(10^mult-x)
