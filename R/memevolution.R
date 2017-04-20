e996b15264c63fe414739c4a948511b6aeb663b4
Jose Eugenio Lozano Alonso <LozAloJo@jcyl.red>
2017-04-20 12:50:01 +0200

    Fixed error

--- R/memevolution.R
+++ R/memevolution.R
@@ -64,7 +64,7 @@ memevolution<-function(i.data, i.evolution.seasons=10, i.evolution.method="seque
         datos.modelo<-memmodel(i.data[indices.modelo], i.seasons=NA, ...)
         evolution.data.i<-c(datos.modelo$n.seasons,
                             datos.modelo$ci.length[1,],
-                            datos.modelo$ci.start[2,],
+                            datos.modelo$ci.start[1,],
                             datos.modelo$ci.percent,
                             datos.modelo$epidemic.thresholds,
                             datos.modelo$intensity.thresholds)
@@ -95,7 +95,7 @@ memevolution<-function(i.data, i.evolution.seasons=10, i.evolution.method="seque
         datos.modelo<-memmodel(i.data[indices.modelo], i.seasons=NA, ...)
         evolution.data.i<-c(datos.modelo$n.seasons,
                             datos.modelo$ci.length[1,],
-                            datos.modelo$ci.start[2,],
+                            datos.modelo$ci.start[1,],
                             datos.modelo$ci.percent,
                             datos.modelo$epidemic.thresholds,
                             datos.modelo$intensity.thresholds)
