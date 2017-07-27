f3fd0c09937078e29b20786dc9585d94bb11d594
Jose Eugenio Lozano Alonso <LozAloJo@jcyl.red>
2017-07-27 14:17:38 +0200

    Error x==0 was x==1 (odd=Inf)

--- R/transformseries.odd.R
+++ R/transformseries.odd.R
@@ -3,7 +3,7 @@
 #' @keywords internal
 transformseries.odd<-function(x, mult=0){
   x[is.nan(x)]<-NA
-  errdat<-!is.na(x) & (is.infinite(x) | x==0)
+  errdat<-!is.na(x) & (is.infinite(x) | x==1)
   x[errdat]<-NA
   if (all(is.na(x))) xt<-x else xt<-x/(10^mult-x)
   xts<-suavizado(xt, hsuav=-1)
