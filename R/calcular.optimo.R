58e5904454356baaa572458d77221ab6e2a89587
Jose Eugenio Lozano Alonso <LozAloJo@jcyl.red>
2018-04-06 12:33:59 +0200

    Fixed minor errors in parametrization of optimum

--- R/calcular.optimo.R
+++ R/calcular.optimo.R
@@ -3,14 +3,13 @@
 #' @keywords internal
 calcular.optimo<-function(i.curva.map,i.metodo,i.parametro){
   # Metodo 1: Original, segunda derivada
-  if (i.metodo==1) resultados<-calcular.optimo.original(i.curva.map,i.parametro)
+  if (i.metodo==1) resultados<-calcular.optimo.original(i.curva.map)
   # Metodo 3: Usando la pendiente de la derivada.
   else if (i.metodo==3) resultados<-calcular.optimo.pendiente(i.curva.map)
   # Metodo 4: Segunda derivada, igualando a 0
   else if (i.metodo==4) resultados<-calcular.optimo.derivada(i.curva.map)
   # Metodo 2: Usando un criterio de % sobre la pendiente
-  else resultados<-calcular.optimo.criterio(i.curva.map,i.parametro)
+  else resultados<-calcular.optimo.criterio(i.curva.map, i.parametro)
   # Metodo 5: Segun Raven
-  #if (i.metodo==5) resultados<-calcular.optimo.varadhan(i.curva.map,i.parametro)
   return(resultados)
 }
--- R/calcular.optimo.original.R
+++ R/calcular.optimo.original.R
@@ -1,13 +1,13 @@
 #' calculates optimum: original method: second derivative + axis change
 #'
 #' @keywords internal
-calcular.optimo.original<-function(i.curva.map,i.hsuav){
+calcular.optimo.original<-function(i.curva.map){
   x<-i.curva.map[,1]
   y<-i.curva.map[,2]
   y.d<-diff(y)
   x.d<-x[-length(x)]
   #y.s<-loess(y.d~x.d)$fitted
-  y.s<-suavizado(y.d,0.6)
+  y.s<-suavizado(y.d)
   x.n<-normalizar(x.d)
   y.n<-normalizar(y.s)
   u<-(x.n-y.n)/sqrt(2)
