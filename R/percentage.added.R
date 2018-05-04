d9e668ee9917e60ebd47f93125e95ba893b933ce
Jose Eugenio Lozano Alonso <LozAloJo@jcyl.red>
2018-05-04 14:30:30 +0200

    Added new parameters to multiple waves transformation

--- R/percentage.added.R
+++ R/percentage.added.R
@@ -4,8 +4,8 @@
 #'
 #' @importFrom RcppRoll roll_sum
 percentage.added<-function(i.data, n){
-  rs<-roll_sum(i.data, n)
+  rs<-roll_sum(i.data, n, na.rm=T)
   rs.max <- which.max(rs)
-  per<-min(i.data[rs.max:(rs.max+n-1)])/sum(i.data[rs.max:(rs.max+n-1)])
-  return(data.frame(percentage=per, start=rs.max, end=rs.max+n-1, n=n, sum=sum(i.data[rs.max:(rs.max+n-1)]), max=rs.max-1+which.max(i.data[rs.max:(rs.max+n-1)]), stringsAsFactors = F))
+  per<-min(i.data[rs.max:(rs.max+n-1)], na.rm=T)/sum(i.data[rs.max:(rs.max+n-1)], na.rm=T)
+  return(data.frame(percentage=per, start=rs.max, end=rs.max+n-1, n=n, sum=sum(i.data[rs.max:(rs.max+n-1)], na.rm=T), max=rs.max-1+which.max(i.data[rs.max:(rs.max+n-1)]), stringsAsFactors = F))
 }
--- R/transformseries.multiple.R
+++ R/transformseries.multiple.R
@@ -8,7 +8,26 @@
 #' @importFrom tidyr spread
 #' @importFrom utils tail
 #' @importFrom ggthemes solarized_pal
-transformseries.multiple <- function(i.data, i.max.duration=30, i.max.waves=NA, i.param.1=0.028, i.param.2=0.015, i.separation=3, i.output=NA){
+transformseries.multiple <- function(i.data, i.max.duration=NA, i.waves=NA, i.param.1=0.028, i.param.2=0.028, i.min.separation=2, i.output=NA, i.force.loess=F){
+  
+  if (is.na(i.max.duration)) max.duration<-NROW(i.data) else max.duration<-i.max.duration
+  
+  if (length(i.waves)==1){
+    if (!is.na(i.waves)){
+      min.waves<-i.waves
+      max.waves<-i.waves
+    }else{
+      min.waves<-1
+      max.waves<-2*NCOL(i.data)
+    }
+  }else if (length(i.waves)==2){
+    if (!is.na(i.waves[1])) min.waves<-i.waves[1] else min.waves<-1
+    if (!is.na(i.waves[2])) max.waves<-max(min.waves, i.waves[2]) else max.waves<-max(min.waves, 2*NCOL(i.data))
+  }else{
+    min.waves<-1
+    max.waves<-2*NCOL(i.data)  
+  }
+
   yrweek <- season <- year <- week <- NULL
   data <- transformdata.back(i.data)$data %>%
     dplyr::arrange(yrweek) %>%
@@ -21,7 +40,9 @@ transformseries.multiple <- function(i.data, i.max.duration=30, i.max.waves=NA, 
                   rates.loess=predict(model.loess, newdata=data$n),
                   rates.filled=if_else(is.na(rates), rates.loess, rates)) %>%
     dplyr::select(-rates) %>%
-    dplyr::filter(!is.na(rates.loess))
+    #dplyr::filter(!is.na(rates.loess)) %>%
+    dplyr::arrange(n)
+  if (i.force.loess) data.plus$rates.filled<-data.plus$rates.loess
   if (!is.na(i.output)){
     outputdir<-file.path(getwd(), i.output)
     if (!dir.exists(outputdir)) dir.create(outputdir)
@@ -46,17 +67,15 @@ transformseries.multiple <- function(i.data, i.max.duration=30, i.max.waves=NA, 
       theme(plot.title = element_text(hjust = 0.5))
     ggsave("Loess.png", p1,  width = 16, height = 9, dpi=150, path=outputdir)
   }
-  if (is.na(i.max.waves)) i.max.waves<-2*NCOL(i.data)
+  # if (is.na(i.max.waves)) i.max.waves<-2*NCOL(i.data)
   results <- data.frame()
   data.plot <- data.frame()
   data.temp <- data.plus
-  difcumsumper<-1
-  j=1
-  while(difcumsumper>i.param.2 & j<i.max.waves+1){
-    cat("iteration ", j, "\n")
-    peradd<-as.data.frame(matrix(unlist(sapply(1:i.max.duration, percentage.added, i.data=data.temp$rates.filled)), ncol=6, byrow = T), stringsAsFactors = F)
+  plots <- list()
+  for(j in 1:max.waves){
+    peradd<-as.data.frame(matrix(unlist(sapply(1:max.duration, percentage.added, i.data=data.temp$rates.filled)), ncol=6, byrow = T), stringsAsFactors = F)
     names(peradd)<-c("percentage","start","end","duration", "sum", "max")
-    n.chosen=tail((1:i.max.duration)[peradd$percentage>i.param.1], 1)
+    n.chosen=tail((1:max.duration)[peradd$percentage>i.param.1], 1)
     peradd.chosen<-data.frame(iteration=j, percentage.added(data.temp$rates.filled, n.chosen))
     results<-bind_rows(results, peradd.chosen)
     data.plot<-rbind(data.plot,
@@ -64,7 +83,7 @@ transformseries.multiple <- function(i.data, i.max.duration=30, i.max.waves=NA, 
     )
     if (!is.na(i.output)){
       n <- rates.filled <- x <- y <- NULL
-      p2 <- ggplot() +
+      plots[[j]] <- ggplot() +
         geom_line(data=data.plus, aes(x=n, y=rates.filled), color="#A0A0A0") +
         geom_point(data=data.plot, aes(x=x, y=y, color=factor(iteration)), size=4) +
         scale_colour_manual(values = colorRampPalette(solarized_pal()(8))(j), guide = guide_legend(nrow=2)) +
@@ -73,22 +92,29 @@ transformseries.multiple <- function(i.data, i.max.duration=30, i.max.waves=NA, 
         labs(title = "Series and epidemics", x = "Week", y = "Data") +
         ggthemes::theme_few() +
         theme(plot.title = element_text(hjust = 0.5))
-      ggsave(paste0("Iteration ",j,".png"), p2,  width = 16, height = 9, dpi=150, path=outputdir)
     }
     sum <- cumsumper <- NULL
     data.temp$rates.filled[peradd.chosen$start:peradd.chosen$end]<-NA
     results <- results %>%
       dplyr::mutate(sumcum=cumsum(sum), cumsumper=cumsum(sum)/sum(data.plus$rates.filled, na.rm=T)) %>%
       dplyr::mutate(difcumsumper= cumsumper - lag(cumsumper))
-    if (j>1) difcumsumper<-results$difcumsumper[NROW(results)]
+    results$difcumsumper[1]<-1
+    cat("iteration ", j,"-",results$difcumsumper[NROW(results)], "\n")
     j<-j+1
-    cat(j,"-",difcumsumper,"\n")
   }
+  
+  max.waves.dif <- max(min.waves, min(results$iteration[results$difcumsumper<i.param.2][1]-1, max.waves, na.rm=T), ra.rm=T)
+
+  if (!is.na(i.output)) for(j in 1:max.waves.dif) ggsave(paste0("Iteration ",j,".png"), plots[[j]],  width = 16, height = 9, dpi=150, path=outputdir)
+
+  results <- results %>%
+    filter(iteration<=max.waves.dif)
+  
   iteration <- start <- end <- n <- sum <- lend <- dif <- unite <- sunite <- x <- from <- to <- NULL
   temp1 <- results %>%
     dplyr::select(iteration, start, end, n, sum) %>%
     dplyr::arrange(start) %>%
-    dplyr::mutate(lend=lag(end), dif=(start-lend-1), unite=(is.na(dif) | dif>=i.separation), sunite=cumsum(unite))
+    dplyr::mutate(lend=lag(end), dif=(start-lend-1), unite=(is.na(dif) | dif>=i.min.separation), sunite=cumsum(unite))
   data.plot.united <- data.plot %>%
     dplyr::left_join(dplyr::select(temp1, iteration, sunite), by="iteration") %>%
     dplyr::mutate(iteration=sunite) %>%
@@ -100,7 +126,7 @@ transformseries.multiple <- function(i.data, i.max.duration=30, i.max.waves=NA, 
     dplyr::select(iteration=sunite, start, end) %>%
     dplyr::arrange(start) %>%
     dplyr::ungroup() %>%
-    dplyr::mutate(from=1+lag(end), to=start-1) %>%
+    dplyr::mutate(from=1+lag(end), to=start) %>%
     dplyr::slice(-1) %>%
     dplyr::select(from, to)
   cut.united<- data.frame()
