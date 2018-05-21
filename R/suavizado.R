8b1345b0a12374b60bc495545bb40a7270d71bbf
Jose Eugenio Lozano Alonso <LozAloJo@jcyl.red>
2018-05-21 14:44:39 +0200

    Fixed error in sm::h.select when length is 1

--- R/suavizado.R
+++ R/suavizado.R
@@ -2,14 +2,19 @@
 #'
 #' @keywords internal
 #' @importFrom sm sm.regression h.select
-suavizado<-function(i.datos,hsuav=-1){
+suavizado<-function(i.datos, hsuav=-1){
   m<-length(i.datos)
   datosx<-(1:m)[!is.na(i.datos)]
   datosy<-i.datos[!(is.na(i.datos) | is.infinite(i.datos))]
-  if (hsuav==-1) hsuav<-h.select(datosx,datosy)
-  smr<-sm.regression(datosx,datosy,h=hsuav,display="none",eval.points=1:m)
-  salida<-smr$estimate
-  salida[salida<0]<-0
-  salida[is.nan(salida)]<-NA
+  # When there is only one point, h.select produces an error
+  if (length(datosx)>1){
+    if (hsuav==-1) hsuav <- h.select(datosx, datosy)
+    smr<-sm.regression(datosx,datosy,h=hsuav,display="none",eval.points=1:m)
+    salida<-smr$estimate
+    salida[salida<0]<-0
+    salida[is.nan(salida)]<-NA    
+  }else{
+    salida <- i.datos
+  }
   return(salida)
 }
